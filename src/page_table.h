9862a6a0d98cdc0a5b849e729c5b94d6d49e2de6
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-08 22:07:36 +0700

    Update page_table.h

---
+++ src/page_table.h
@@ -0,0 +1,15 @@
+#ifndef PAGE_TABLE_H
+#define PAGE_TABLE_H
+
+typedef struct {
+    int id;
+    char data[256];
+} Page;
+
+void initialize_page_table(int size);
+int translate_address(int virtual_address);
+void update_page_table(int page_id, int frame_id);
+Page *get_page(int page_id);
+void load_page(int page_id, const char *data);
+
+#endif // PAGE_TABLE_H
