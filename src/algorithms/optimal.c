2a3489120e354d7284e4840185a5c5a3a38314c1
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-08 22:03:27 +0700

    Update optimal.c

---
+++ src/algorithms/optimal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "optimal.h"
+
+static int *memory = NULL;
+static int memory_size = 0;
+static int *access_sequence = NULL;
+static int sequence_length = 0;
+
+void initialize_optimal(int capacity, int *sequence, int sequence_len) {}
+
+int optimal_choose_page_to_replace(int current_index) {}
+
+void optimal_add_page(int page) {}
