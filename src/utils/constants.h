4f335b5c11c3f76689e8dc4985e1aefd4d873138
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-08 22:14:31 +0700

    Update constants.h

---
+++ src/utils/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MEMORY_SIZE 4  // Number of frames in memory
+#define MAX_PAGES 100  // Maximum number of pages supported
+
+#endif // CONSTANTS_H
