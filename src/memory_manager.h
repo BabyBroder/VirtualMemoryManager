58eb7cfe564356ec2fb3570d220bac54f28dd5a9
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 01:33:35 +0700

    Update memory_manager.h

--- src/memory_manager.h
+++ src/memory_manager.h
@@ -1,9 +1,22 @@
 #ifndef MEMORY_MANAGER_H
 #define MEMORY_MANAGER_H
 
-void initialize_memory(int size);
-void handle_page_fault(int page_id);
-void add_page_to_memory(int page_id);
-int is_page_in_memory(int page_id);
+#include <stdint.h>
+#include <stdbool.h>
+#include "page_table.h"
+#include "tlb.h"
+
+// Function to initialize memory management components
+void initialize_memory_manager(PageTable *page_table, TLB *tlb, size_t num_frames);
+
+// Function to translate a virtual address to a physical address
+// Returns -1 if the address translation fails (page fault)
+int translate_address(PageTable *page_table, TLB *tlb, uint32_t virtual_address);
+
+// Function to load a page into memory
+void load_page(PageTable *page_table, TLB *tlb, uint16_t page_id, const char *data);
+
+// Function to evict a page from memory using the TLB
+void evict_page(PageTable *page_table, TLB *tlb, uint16_t page_id);
 
 #endif // MEMORY_MANAGER_H
