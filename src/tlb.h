6e19ad1301e89e9c5a48545074083a24a2018037
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 00:46:46 +0700

    Update tlb.h

--- src/tlb.h
+++ src/tlb.h
@@ -1,9 +1,22 @@
 #ifndef TLB_H
 #define TLB_H
 
-void initialize_tlb();
+#include <stdbool.h>
+
+typedef struct {
+    int virtual_address;
+    int physical_address;
+    bool valid;
+} TLBEntry;
+
+void initialize_tlb(int tlb_size);
+
 int lookup_tlb(int virtual_address);
+
 void update_tlb(int virtual_address, int physical_address);
+
 void invalidate_tlb_entry(int virtual_address);
+
+void clear_tlb();
 
 #endif // TLB_H
