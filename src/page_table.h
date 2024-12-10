c1bf607732de36f46f0cde6f3b49bd17814d6524
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 01:09:22 +0700

    Update page_table.h

--- src/page_table.h
+++ src/page_table.h
@@ -1,14 +1,32 @@
 #ifndef PAGE_TABLE_H
 #define PAGE_TABLE_H
-typedef struct {
-    int id;
-    char data;
-} Page;
 
-void initialize_page_table(int size);
-int translate_address(int virtual_address);
-void update_page_table(int page_id, int frame_id);
-Page *get_page(int page_id);
-void load_page(int page_id, const char *data);
+#include <stdint.h>
+#include <stdbool.h>
+
+// Structure for a page table entry
+typedef struct {
+    uint16_t frame_number; // Frame number in physical memory
+    bool valid;            // Valid bit to indicate if the page is in memory
+} PageTableEntry;
+
+// Structure for the page table
+typedef struct {
+    PageTableEntry *entries; // Array of page table entries
+    size_t num_pages;        // Total number of pages
+} PageTable;
+
+// Initializes the page table
+void initialize_page_table(PageTable *page_table, size_t num_pages);
+
+// Translates a logical page number to a frame number
+// Returns -1 if the page is not valid (i.e., a page fault)
+int translate_address(PageTable *page_table, uint16_t page_number);
+
+// Updates the page table when a page is loaded into memory
+void update_page_table(PageTable *page_table, uint16_t page_number, uint16_t frame_number);
+
+// Cleans up resources allocated for the page table
+void free_page_table(PageTable *page_table);
 
 #endif // PAGE_TABLE_H
