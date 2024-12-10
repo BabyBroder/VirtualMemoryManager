e251f25ff57472d0107a2141de22a99dd9cf5310
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 01:34:14 +0700

    Update memory_manager.c

--- src/memory_manager.c
+++ src/memory_manager.c
@@ -2,16 +2,22 @@
 #include <stdlib.h>
 #include "memory_manager.h"
 #include "page_table.h"
-#include "disk_manager.h"
+#include "tlb.h"
+#include "constants.h"
 
-static int *memory = NULL;
-static int memory_size = 0;
-static int memory_used = 0;
+// Private global variables
+static Page *frames = NULL;
+static int frame_count = 0;
 
-void initialize_memory(int size) {}
+// Initialize memory management components
+void initialize_memory_manager(PageTable *page_table, TLB *tlb, size_t num_frames) {}
 
-void handle_page_fault(int page_id) {}
+// Translate a virtual address to a physical address
+// Returns -1 if the address translation fails (page fault)
+int translate_address(PageTable *page_table, TLB *tlb, uint32_t virtual_address) {}
 
-void add_page_to_memory(int page_id) {}
+// Load a page into memory
+void load_page(PageTable *page_table, TLB *tlb, uint16_t page_id, const char *data) {}
 
-int is_page_in_memory(int page_id) {}
+// Evict a page from memory using the TLB
+void evict_page(PageTable *page_table, TLB *tlb, uint16_t page_id) {}
