71b6a7d80ff56feb9bafb5aa3ce410b63b19c927
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-10 16:50:01 +0700

    Update main.c

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "memory_manager.h"
+#include "page_table.h"
+#include "disk_manager.h"
+#include "algorithms/fifo.h" 
+#include "utils/parser.h"
+#include "utils/logger.h"
+#include "utils/constants.h"
+
+
