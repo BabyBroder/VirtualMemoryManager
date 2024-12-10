97e3e2ca513bdd5d65f0104df6706e3b5b1086e8
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 00:04:14 +0700

    Update optimal.h

--- src/algorithms/optimal.h
+++ src/algorithms/optimal.h
@@ -3,6 +3,6 @@
 
 void initialize_optimal(int capacity, int *sequence, int sequence_length);
 int optimal_choose_page_to_replace(int current_index);
-void optimal_add_page(int page);
-
+void optimal_add_page(int page, int current_index);
+void free_optimal_memory();
 #endif // OPTIMAL_H
