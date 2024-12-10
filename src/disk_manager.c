2d80c2dd8b522e6c793b89f05d1f067c0c1f3848
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 01:47:01 +0700

    Update disk_manager.c

--- src/disk_manager.c
+++ src/disk_manager.c
@@ -2,23 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include "disk_manager.h"
+#include "page_table.h"
+#include "tlb.h"
+#include "memory_manager.h"
 
-#define MAX_DISK_PAGES 100
+// Initializes the disk
+void initialize_disk(Disk *disk) {}
 
-typedef struct {
-    int id;
-    char data[256];
-} DiskPage;
+// Reads a page from the disk
+// Returns the data of the page or NULL if the page is not in the disk
+char* read_page_from_disk(Disk *disk, uint16_t page_id) {}
 
-static DiskPage disk[MAX_DISK_PAGES];
-static int disk_size = 0;
+// Writes a page to the disk
+void write_page_to_disk(Disk *disk, uint16_t page_id, const char *data) {}
 
-void initialize_disk() {}
+// Swaps in a page from the disk into memory
+void swap_in_page(Disk *disk, PageTable *page_table, TLB *tlb, int page_number, char *page_data) {}
 
-void swap_out_page(Page *page) {}
+// Swaps out a page from memory to disk
+void swap_out_page(Disk *disk, PageTable *page_table, TLB *tlb, int page_number) {}
 
-void swap_in_page(int page_id) {}
+// Checks if a page is on the disk
+bool is_page_on_disk(Disk *disk, int page_number) {}
 
-int is_page_on_disk(int page_id) {}
-
-int disk_is_empty() {}
+// Checks if the disk is empty
+bool is_disk_empty(Disk *disk) {}
