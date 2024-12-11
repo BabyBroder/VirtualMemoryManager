5efd376a1bc11d42ec3a1503174332389c6efb8e
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 19:07:06 +0700

    Update physical_memory.h

--- src/physical_memory.h
+++ src/physical_memory.h
@@ -13,15 +13,19 @@ typedef struct{
 typedef struct {
     Frame frames[TOTAL_FRAMES];
     int nums_frames;
+    Algorithm algorithm;
+    AlgorithmStruct algorithm_struct;
 } PhysicalMemory;
 
 
-void initialize_physical_memory(PhysicalMemory *physical_memory);
+void initialize_physical_memory(PhysicalMemory *physical_memory, Algorithm algorithm);
+
+void add_page_to_physical_memory(PhysicalMemory *physical_memory, int page, int current_index);
 
 int read_from_physical_memory(PhysicalMemory *physical_memory, uint32_t physical_address, char *buffer, int size);
 
 int write_to_physical_memory(PhysicalMemory *physical_memory, uint32_t physical_address, const char *buffer, int size);
 
-int find_free_frame(PhysicalMemory *physical_memory, Algorithm algorithm);
+int find_free_frame(PhysicalMemory *physical_memory, int current_index);
 
 #endif // PHYSICAL_MEMORY_H
