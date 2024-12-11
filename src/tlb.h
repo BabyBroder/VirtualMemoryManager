4a4063977bcb80698a9de627763bd1735cfac9f2
Ahn <23520027@gm.uit.edu.vn>
2024-12-11 20:15:06 +0700

    update tlb

--- src/tlb.h
+++ src/tlb.h
@@ -23,14 +23,18 @@ typedef struct {
 } TLB;
 
 // Initializes the TLB
-void initialize_tlb(TLB *tlb, int replacement);
+void initialize_tlb(TLB *tlb, Algorithm algorithm);
 
+int choose_entry_to_replace(TLB *tlb, uint16_t page_number, uint16_t frame_number, int current_index);
+
+bool add_entry_to_replacment(TLB *tlb, uint16_t page_number, uint16_t frame_number, int current_index);
+
+void tlb_add_entry(TLB *tlb, uint16_t page_number, uint16_t frame_number, int current_index);
+// Adds an entry to the TLB (replaces an existing one if necessary)
+
+int tlb_lookup(TLB *tlb, uint16_t page_number);
 // Looks up the TLB for a given page number
 // Returns the frame number on a hit, -1 on a miss
-int tlb_lookup(TLB *tlb, uint16_t page_number);
-
-// Adds an entry to the TLB (replaces an existing one if necessary)
-void tlb_add_entry(TLB *tlb, uint16_t page_number, uint16_t frame_number);
 
 // Prints the contents of the TLB (for debugging or logging)
 void print_tlb(const TLB *tlb);
