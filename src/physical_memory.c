762ad7c1d2c6e04e406684abfec0a1cac2fda6fc
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 15:15:53 +0700

    Create physical_memory.c

---
+++ src/physical_memory.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "physical_memory.h"
+#include "utils/constants.h"
+
+void initialize_physical_memory(PhysicalMemory *physical_memory){}
+
+int read_from_physical_memory(PhysicalMemory *physical_memory, uint32_t physical_address, char *buffer, int size){}
+
+int write_to_physical_memory(PhysicalMemory *physical_memory, uint32_t physical_address, const char *buffer, int size){}
