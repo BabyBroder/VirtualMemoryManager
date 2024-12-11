c9c66d0d501edba59a064cd20cf8de196b436307
miscitaofvh <nguyenvanhung14112005@gmail.com>
2024-12-11 15:57:52 +0700

    update fifo.h

--- src/algorithms/fifo.h
+++ src/algorithms/fifo.h
@@ -7,9 +7,9 @@ typedef struct {
     int front, rear, size;
 } FIFO;
 
-void initialize_fifo(FIFO structure, int capacity);
-int fifo_choose_page_to_replace(FIFO structure);
-void fifo_add_page(FIFO structure, int page);
-void free_fifo(FIFO structure);
+void initialize_fifo(FIFO* structure, int capacity);
+int fifo_choose_page_to_replace(FIFO* structure);
+void fifo_add_page(FIFO* structure, int page);
+void free_fifo(FIFO* structure);
 
 #endif // FIFO_H
