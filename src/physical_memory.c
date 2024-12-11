de8b5e7bf6f9e6a270ce5108ff00657f6124c157
Broder <158417266+BabyBroder@users.noreply.github.com>
2024-12-11 15:33:05 +0700

    Update physical_memory.c

--- src/physical_memory.c
+++ src/physical_memory.c
@@ -10,3 +10,5 @@ void initialize_physical_memory(PhysicalMemory *physical_memory){}
 int read_from_physical_memory(PhysicalMemory *physical_memory, uint32_t physical_address, char *buffer, int size){}
 
 int write_to_physical_memory(PhysicalMemory *physical_memory, uint32_t physical_address, const char *buffer, int size){}
+
+int find_free_frame(){}
