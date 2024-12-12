7dacb6ca9206524f54072c65e76db84039b59b94
BabyBroder <iluvstudyforever@gmail.com>
2024-12-12 09:58:29 +0000

    Update page_table

--- src/page_table.h
+++ src/page_table.h
@@ -9,6 +9,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "utils/constants.h"
+#include "algorithms/algorithm.h"
+#include "utils/constants.h"
+#include "virtual_memory.h"
 #include "physical_memory.h"
 
 
@@ -61,14 +64,6 @@ void initialize_page_table(PageTable *page_table);
  */
 int page_table_lookup(PageTable *page_table, uint16_t page_number);
 
-/**
- * Translates a logical page number to a frame number.
- *
- * @param page_table A pointer to the PageTable structure.
- * @param virtual_address The virtual address to be translated.
- * @return The frame number corresponding to the virtual address, or -1 if the page is not valid (i.e., a page fault).
- */
-int translate_address(PageTable *page_table, uint32_t virtual_address);
 
 /**
  * Updates the page table when a page is loaded into memory.
@@ -77,27 +72,7 @@ int translate_address(PageTable *page_table, uint32_t virtual_address);
  * @param page_number The page number to be updated.
  * @param frame_number The frame number to be associated with the page number.
  */
-void update_page_table(PageTable *page_table, uint16_t page_number, uint16_t frame_number);
-
-/**
- * Gets the page by page number.
- *
- * @param page_table A pointer to the PageTable structure.
- * @param page_id The page number to retrieve.
- * @return A pointer to the Frame structure corresponding to the page number.
- */
-Frame *get_page(PageTable *page_table, uint16_t page_id);
-
-/**
- * Loads a page into memory.
- *
- * @param physical_memory A pointer to the PhysicalMemory structure.
- * @param page_table A pointer to the PageTable structure.
- * @param page_id The page number to be loaded.
- * @param data A pointer to the data to be loaded into the page.
- * @param current_index The current index in the physical memory where the page will be loaded.
- */
-void load_page(PhysicalMemory *physical_memory, PageTable *page_table, uint16_t page_id, const char *data, int current_index);
+void page_table_add_entry(PageTable *page_table, uint16_t page_number, uint16_t frame_number);
 
 /**
  * Cleans up resources allocated for the page table.
@@ -105,6 +80,6 @@ void load_page(PhysicalMemory *physical_memory, PageTable *page_table, uint16_t 
  * @param physical_memory A pointer to the PhysicalMemory structure associated with the page table.
  * @param page_table A pointer to the PageTable structure to be freed.
  */
-void free_page_table(PhysicalMemory* physical_memory, PageTable *page_table) ;
+void free_page_table(PageTable *page_table) ;
 
 #endif // PAGE_TABLE_H
