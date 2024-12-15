c8689f9b98923a7ef9fe48dee7c79a292aeae488
Ahn <23520027@gm.uit.edu.vn>
2024-12-15 21:35:34 +0700

    update translate address

--- src/main.c
+++ src/main.c
@@ -1,3 +1,16 @@
-
+#include"main.h"
+int translate_virtual_to_physical(TLB *tlb, PageTable *page_table, uint16_t virtual_address)
+{
+    uint8_t page_frame = (virtual_address >> 8) && PAGE_SIZE;
+    
+    if (tlb_lookup(tlb,page_frame) != -1) 
+        return tlb_lookup(tlb,page_frame);
+    
+    return page_table_lookup(page_table, page_frame);
+}
+int main()
+{
+    return 0;
+}
 
 
