6f2038f54db0434e56c8044a088c9037b7ed28f0
Ahn <23520027@gm.uit.edu.vn>
2024-12-15 18:18:14 +0700

    change create backing store into binary file

--- create_data/create_physical_addresses.c
+++ create_data/create_physical_addresses.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main()
-{
-    srand(time(NULL));
-    freopen("../data/physical_addresses.txt", "w", stdout);
 
-    for(int i=0 ; i<256; i++) 
-        {
-            for(int offset = 0 ; offset<256; offset++)
-            {
-                int num = (i<<8)|offset; 
-                int signed_byte = -1*rand()%127;
-                if((rand()*i-9)%2==0){
-                signed_byte*=-1;
-                printf("%d %d\n",num,signed_byte);
+int main() {
+    srand(time(NULL));
+    FILE *backing_store;
+    backing_store = fopen("../data/BACKING_STORE.bin", "wb"); // Open for binary write
+
+    if (backing_store == NULL) {
+        perror("Failed to open backing store file");
+        return 1;
+    }
+
+    for(int i = 0; i < 256; i++) {
+        for(int offset = 0; offset < 256; offset++) {
+            int num = (i << 8) | offset; 
+            int signed_byte = -1 * (rand() % 127);
+            if ((rand() * i - 9) % 2 == 0) {
+                signed_byte *= -1;
             }
+            // Writing to the binary file
+            fwrite(&num, sizeof(int), 1, backing_store);
+            fwrite(&signed_byte, sizeof(int), 1, backing_store);
         }
     }
+
+    fclose(backing_store);
+
+    printf("BACKING_STORE.bin file has been generated successfully.\n");
+
     return 0;
 }
