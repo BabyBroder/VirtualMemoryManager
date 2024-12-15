0ab66d41b58997a7cc57f4d2fd74756fa222f6a1
Ahn <23520027@gm.uit.edu.vn>
2024-12-15 19:37:44 +0700

    small addresses.txt

--- create_data/create_logical_addresses.c
+++ create_data/create_logical_addresses.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+const int num_line = 10;
+const int range = 65536;
+const char* file_test = "../test/addresses.txt";
 int main()
 {
     srand(time(NULL));
-    freopen("../data/addresses.txt","w",stdout);
-    for(int i = 0 ;i<=1000; i++) 
-        {
-            int num = rand()%65536; 
-        // tạo logical addresss bao gồm virtual address + offsets  
-            printf("%d\n",num);
-        }
+    freopen(file_test, "w", stdout);
+    for(int i = 0; i < num_line; i++) {
+        int offset = rand() % 256;
+        int num = (i << 8) | offset;
+        printf("%d\n", num);
+    }
     return 0;
 }
