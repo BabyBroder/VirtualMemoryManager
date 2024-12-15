f46c17674f76b400b24f851a5a02e5eb7ca93e8b
Ahn <23520027@gm.uit.edu.vn>
2024-12-15 21:39:56 +0700

    main change

--- src/main.c
+++ src/main.c
@@ -1,12 +1,7 @@
 #include"main.h"
-int translate_virtual_to_physical(TLB *tlb, PageTable *page_table, uint16_t virtual_address)
+int get_physical_address(uint8_t frame_number, uint8_t offset)
 {
-    uint8_t page_frame = (virtual_address >> 8) && PAGE_SIZE;
-    
-    if (tlb_lookup(tlb,page_frame) != -1) 
-        return tlb_lookup(tlb,page_frame);
-    
-    return page_table_lookup(page_table, page_frame);
+    return frame_number * FRAME_SIZE + offset;
 }
 int main()
 {
