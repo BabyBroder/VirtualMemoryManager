327231f0dd687f83448b918113c3be7462effd36
Nghia2123 <23521018@gm.uit.edu.vn>
2024-12-15 15:25:17 +0700

    First test test_page_table.c (failed)

--- test/test_page_table.c
+++ test/test_page_table.c
@@ -1,6 +1,14 @@
-#include "../src/main.h"
+#include "../lib/structure/page_table.h"
 
 int main()
 {
-    return 0;
+    PageTable *page_table;
+    page_table = (PageTable *)malloc(sizeof(PageTable));
+    initialize_page_table(page_table);
+
+    link_page_table_to_frame(page_table, 1, 2);
+    link_page_table_to_frame(page_table, 2, 3);
+
+    printf("Page table lookup for page 1: %d\n", page_table_lookup(page_table, 1));
+    printf("Page table lookup for page 2: %d\n", page_table_lookup(page_table, 2));
 }
